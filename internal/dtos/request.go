18c1c5a6940505f14ae3cd61e8e1e2904c4db3c6
Asher Siddique <ashersiddique127@gmail.com>
2026-02-12 00:56:45 +0500

    Add a DTO for binding incoming requests

--- internal/dtos/request.go
+++ internal/dtos/request.go
@@ -1,1 +1,22 @@
 package dtos
+
+type ReadmeResponseDTO struct {
+	Type        string  `json:"type"`
+	Encoding    string  `json:"encoding"`
+	Size        int     `json:"size"`
+	Name        string  `json:"name"`
+	Path        string  `json:"path"`
+	Content     string  `json:"content"`
+	Sha         string  `json:"sha"`
+	Url         string  `json:"url"`
+	GitUrl      *string `json:"git_url,omitempty"`
+	HtmlUrl     *string `json:"html_url,omitempty"`
+	DownloadUrl *string `json:"download_url,omitempty"`
+	Links       struct {
+		Git  *string `json:"git"`
+		Html *string `json:"html"`
+		Self string  `json:"self"`
+	} `json:"_links"`
+	Target          *string `json:"target,omitempty"`
+	SubmoduleGitUrl *string `json:"submodule_git_url,omitempty"`
+}
