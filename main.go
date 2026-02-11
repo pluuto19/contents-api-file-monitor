8dc68a504289cc098d84870cf82ea41372165617
Asher Siddique <ashersiddique127@gmail.com>
2026-02-11 23:45:17 +0500

    Set up main process loop

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	ctx, stopFunc := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stopFunc()
+
+	loopTicker := time.NewTicker(15 * time.Minute)
+
+	go startMainLoop(loopTicker, ctx)
+}
+
+func startMainLoop(t *time.Ticker, ctx context.Context) {
+	for {
+		select {
+		case <-t.C:
+		case <-ctx.Done():
+			return
+		}
+	}
 }
